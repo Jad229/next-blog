6d42af9111c853ecfa1a73c96d746cd698410240
Jad229 <jad229@njit.edu>
2024-02-01 14:57:47 -0500

    Update styles and add responsive menu

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,17 +1,41 @@
+'use client'
 import Link from "next/link"
 import styles from "./authLinks.module.css"
+import { useState } from "react"
 export const AuthLinks = () => {
-  const status = "authenticated"
+  const status = "notauthenticated"
 
+  const [open, setOpen] = useState(false);
   return (
     <>
     {status === "notauthenticated" ? (
-    <Link href="/login">Login</Link>
+    <Link className={styles.links} href="/login">Login</Link>
     ) : (
     <>
-      <Link href="/write">Write</Link>
+      <Link className={styles.links} href="/write">Write</Link>
       <span className={styles.links}>Logout</span>
     </>
+    )}
+    <div className={styles.burger} onClick={() => setOpen(!open)}>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+    </div>
+
+    {open && (
+      <div className={styles.responsiveMenu}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/Contact">Contact</Link>
+        {status === "notauthenticated" ? (
+          <Link href="/login">Login</Link>
+          ) : (
+          <>
+            <Link href="/write">Write</Link>
+            <span className={styles.links}>Logout</span>
+          </>
+        )}
+      </div>
     )}
     </>
   )
