ce119efa425578985875caee63602a76fff0197b
Jad229 <jad229@njit.edu>
2024-02-01 14:11:25 -0500

    theme toggler implemented

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,6 +1,25 @@
+'use client'
+import Image from "next/image"
 import styles from "./themeToggle.module.css"
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 export const ThemeToggle = () => {
+  const { toggle,theme } = useContext(ThemeContext);
+
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div className={styles.container} onClick={toggle} style={
+      theme === "dark" 
+      ? { backgroundColor: "white" } 
+      : { backgroundColor: "#0f172a" }}>
+
+      <Image src="/moon.png" alt="theme toggle" width={14} height={14}/>
+      <div className={styles.ball} style={
+          theme === "dark" 
+          ? { left: 2, backgroundColor: "#0f172a" } 
+          : { right: 2, backgroundColor: "white" }}>
+            
+          </div>
+      <Image src="/sun.png" alt="theme toggle" width={14} height={14}/>
+    </div>
   )
 }
